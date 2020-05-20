37271486b6f040e86f66ec1354a041796136eefb
Karthikeyan.Ganesan <info@n2psystems.ca>
2020-05-19 22:23:47 -0400

    Version 2.0
    Signed-off-by: Karthikeyan.Ganesan <info@n2psystems.ca>

--- Testing/__init__.py
+++ Testing/__init__.py
@@ -1,3 +1,40 @@
 print("good morning")
-if 5>2:
+print("Naveen is a good boy")
+if 5 > 2:
     print("Five greater than two")
+
+    # this is a comment
+    """
+    testing
+    """
+    print("good evening")
+    x = 4
+    # is the same as
+    x = 5
+    print(type(x))
+
+x = frozenset(("apple", "banana", "cherry"))
+print(x)
+print(type(x))
+x = bytes(5)
+print(x)
+x = dict(name="John", age=36)
+print(x)
+
+
+def remove_duplicate(lista) -> object:
+    lista2 = []
+    if lista:
+        for item in lista:
+            if item not in lista2:
+                lista2.append(item)
+            else:
+                #print("lista" + lista2)
+                return lista
+            #print("lista" + lista2)
+            return lista2
+
+
+print(x)
+remove_duplicate([2,3,4,3])
+print(x)
